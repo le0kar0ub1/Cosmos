164245cc8ba504cba6536e1aade43362357e504d
leo.karoubi <leokaroubi@gmail.com>
2020-07-24 18:18:05 +0200

    IDT filled

--- inc/arch/x86_64/descriptors/idt.h
+++ inc/arch/x86_64/descriptors/idt.h
@@ -17,22 +17,22 @@
 
 struct x86_64_idt_gate
 {
-   u16_t offset_1  : 16;  // offset bits 0..15
-   u16_t selector  : 16;  // a code segment selector in GDT or LDT
-   u8_t  ist       : 3;   // holds Interrupt Stack Table offset.
-   u8_t  _zero1    : 5;
+   uintptr_t offset_1 : 16;  // offset bits 0..15
+   uintptr_t selector : 16;  // a code segment selector in GDT or LDT
+   uintptr_t ist      : 3;   // holds Interrupt Stack Table offset.
+   uintptr_t _zero1   : 5;
    union {
         struct {
-            u8_t GateType  : 4;
-            u8_t Zero      : 1;
-            u8_t DPL       : 2;
-            u8_t Present   : 1;
+            uintptr_t GateType  : 4;
+            uintptr_t Zero      : 1;
+            uintptr_t DPL       : 2;
+            uintptr_t Present   : 1;
         };
-        u8_t attribs : 8;
+        uintptr_t attribs : 8;
     };
-   u16_t offset_2  : 16;  // offset bits 16..31
-   u32_t offset_3  : 32;  // offset bits 32..63
-   u32_t _zero2    : 32;
+   uintptr_t offset_2 : 16;  // offset bits 16..31
+   uintptr_t offset_3 : 32;  // offset bits 32..63
+   uintptr_t _zero2   : 32;
 } __packed;
 
 static_assert(sizeof(struct x86_64_idt_gate) == 16ul);
@@ -51,5 +51,7 @@ struct x86_64_idt_ptr
 } __packed;
 
 static_assert(sizeof(struct x86_64_idt_ptr) == 10ul);
+
+void idt_init(void);
 
 #endif /* _ARCH_x86_64_IDT_H_ */
--- src/arch/x86_64/descriptors/idt.c
+++ src/arch/x86_64/descriptors/idt.c
@@ -10,18 +10,41 @@
 # include <arch/x86_64/descriptors/idt.h>
 # include <arch/x86_64/descriptors/descriptors.h>
 
+/*
+** Entire X86_64 IDT
+*/
+__aligned(16)
 static struct x86_64_idt idt = {0};
 
+/*
+** Fat idt ptr
+*/
+__aligned(16)
 static struct x86_64_idt_ptr idtptr = {
     .size   = (16 * 256) - 1,
     .offset = &idt,
 };
 
+/*
+** Fill in all gates with the ISR handler & setup types
+*/
 void idt_init(void)
 {
+    extern uintptr_t x86_64_interrupt_handler;
     for (u32_t i = 0; i < 256; i++) {
+        idt.gates[i].offset_1 = (u16_t)(x86_64_interrupt_handler >> 00ul);
+        idt.gates[i].offset_2 = (u16_t)(x86_64_interrupt_handler >> 16ul);
+        idt.gates[i].offset_3 = (u32_t)(x86_64_interrupt_handler >> 32ul);
+
         idt.gates[i].GateType = DESCRIPTORS_INTERRUPT_GATE_64;
-        idt.gates[i].DPL = 0x0;
-        idt.gates[i].Present = 0x1;
+        idt.gates[i].DPL      = DPL_LEVEL_KERNEL;
+        idt.gates[i].Present  = 0x1;
     }
+
+    asm volatile(
+        "lidt (%%eax)"
+        :
+        : "a"(&idtptr)
+        :
+    );
 }
